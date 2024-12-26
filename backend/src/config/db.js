558b386a8264b42ec32ad27eb746075a1b58ed1e
Surendar K <surendarkumar2k@gmail.com>
2024-12-26 20:40:16 +0530

    db file

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -13,22 +13,3 @@ db.connect((err) => {
 });
 
 module.exports = db;
-
-const mysql = require('mysql2');
-
-const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root', // replace with your MySQL username
-  password: 'Santhiya@31', // replace with your MySQL password
-  database: 'authdb',
-});
-
-db.connect((err) => {
-  if (err) {
-    console.error('DB connection error:', err);
-    return;
-  }
-  console.log('Connected to the database');
-});
-
-module.exports = db;
