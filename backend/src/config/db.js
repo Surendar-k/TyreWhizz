fe09f23cb32c3ae34ae9a12629a5a90c51cfa3db
Your Name <your_email@example.com>
2024-12-27 09:40:57 +0530

    db

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,25 +1,9 @@
-const mysql = require('mysql2');
+ mysql = require('mysql2');
 
 const db = mysql.createConnection({
   host: '127.0.0.1',
   user: 'root', // replace with your MySQL username
   password: '123qwe', // replace with your MySQL password
-  database: 'authdb',
-});
-
-db.connect((err) => {
-  if (err) throw err;
-  console.log('Database connected!');
-});
-
-module.exports = db;
-
-const mysql = require('mysql2');
-
-const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root', // replace with your MySQL username
-  password: 'Santhiya@31', // replace with your MySQL password
   database: 'authdb',
 });
 
