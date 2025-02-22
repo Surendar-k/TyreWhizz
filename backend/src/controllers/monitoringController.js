3ee17b39715dda12304cf0148af4f58bc2e97580
Surendar K <surendarkumar2k@gmail.com>
2025-02-22 22:44:14 +0530

    added some logic to avoid undefined sensor data error

--- backend/src/controllers/monitoringController.js
+++ backend/src/controllers/monitoringController.js
@@ -14,12 +14,12 @@ const receiveSensorData = (req, res) => {
     ambient_temp !== undefined
   ) {
     sensorData = {
-      pressure: parseFloat(pressure),
-      incontact_temp: parseFloat(incontact_temp),
-      ambient_temp: parseFloat(ambient_temp),
-      acc_x: acc_x !== undefined ? parseFloat(acc_x) : null, // Handle missing data
-      acc_y: acc_y !== undefined ? parseFloat(acc_y) : null,
-      acc_z: acc_z !== undefined ? parseFloat(acc_z) : null,
+      pressure: parseFloat(pressure) || 0,
+      incontact_temp: parseFloat(incontact_temp) || 0,
+      ambient_temp: parseFloat(ambient_temp) || 0,
+      acc_x: acc_x !== undefined ? parseFloat(acc_x) : 0,
+      acc_y: acc_y !== undefined ? parseFloat(acc_y) : 0,
+      acc_z: acc_z !== undefined ? parseFloat(acc_z) : 0,
     };
 
     console.log("Received sensor data:", sensorData);
@@ -27,11 +27,9 @@ const receiveSensorData = (req, res) => {
     // Respond to ESP32
     res.status(200).json({ message: "Data received", data: sensorData });
   } else {
-    res
-      .status(400)
-      .json({
-        error: "Missing required data (pressure, incontact_temp, ambient_temp)",
-      });
+    res.status(400).json({
+      error: "Missing required data (pressure, incontact_temp, ambient_temp)",
+    });
   }
 };
 
